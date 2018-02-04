77c847f9edf66c3611a3da82a2462a2e33946e63
Edeeson Opina <edeesonopinav4@gmail.com>
2018-02-04 04:29:41 -0800

    Fixed URL links

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,21 +7,21 @@
     <!-- /#wrapper -->
 
     <!-- jQuery -->
-    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="{{ url('vendor/jquery/jquery.min.js') }}"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="{{ url('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="vendor/metisMenu/metisMenu.min.js"></script>
+    <script src="{{ url('vendor/metisMenu/metisMenu.min.js') }}"></script>
 
     <!-- Morris Charts JavaScript -->
-    <script src="vendor/raphael/raphael.min.js"></script>
-    <script src="vendor/morrisjs/morris.min.js"></script>
-    <script src="data/morris-data.js"></script>
+    <script src="{{ url('vendor/raphael/raphael.min.js') }}"></script>
+    <script src="{{ url('vendor/morrisjs/morris.min.js') }}"></script>
+    <script src="{{ url('data/morris-data.js') }}"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="dist/js/sb-admin-2.js"></script>
+    <script src="{{ url('dist/js/sb-admin-2.js') }}"></script>
 
 </body>
 
