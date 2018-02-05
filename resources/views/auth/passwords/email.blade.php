bd2cbfe8f161aea3e3470848a0cc5a28d1d5ac81
Earl <earlrak@gmail.com>
2018-02-05 11:12:20 +0800

    updated

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,9 +1,5 @@
-@extends('layouts.app')
+@include('layouts.header')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Reset Password</div>
                 <div class="panel-body">
@@ -40,7 +36,5 @@
                     </form>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-@endsection
+      
+@include('layouts.footer')
